f6a6a29b7c73208e1e65e11b5997533e7586a1d7
mnasibzade <euoryexe@gmail.com>
2025-02-15 18:38:07 +0100

    config: Introduce MN makefiles and build logic

---
+++ config/mn.mk
@@ -0,0 +1,12 @@
+# TARGET_BUILD_PACKAGE options:
+# 1 - vanilla (default)
+# 2 - microg
+# 3 - gapps
+ifeq ($(TARGET_BUILD_PACKAGE),1)
+  BUILD_GMS_OVERLAYS_AND_PROPS := true
+  $(call inherit-product, vendor/gapps/gapps.mk)
+else
+  ifeq ($(TARGET_BUILD_PACKAGE),2)
+    $(call inherit-product, vendor/microg/product.mk)
+  endif
+endif
