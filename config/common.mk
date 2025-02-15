d011ed2c50001c44579ec8b062926cf599fca906
mnasibzade <euoryexe@gmail.com>
2025-02-15 18:40:28 +0100

    config: Add clocks

--- config/common.mk
+++ config/common.mk
@@ -1,6 +1,7 @@
 # Allow vendor/extra to override any property by setting it first
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/mk.mk)
+$(call inherit-product-if-exists, vendor/clocks/clocks.mk)
 
 PRODUCT_BRAND ?= LineageOS
 
